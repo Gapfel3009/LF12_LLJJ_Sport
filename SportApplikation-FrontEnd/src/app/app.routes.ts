68fa815b48c2c6c694c164afc7c31d5649ff5cc0
J.Ketzler <j.ketzler@affinis.de>
2025-04-04 11:52:26 +0200

    Mainsite erstellt und redirect hin nach anmeldung

--- SportApplikation-FrontEnd/src/app/app.routes.ts
+++ SportApplikation-FrontEnd/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', loadComponent:()=>import('./components/login/login.component').then(c=> c.LoginComponent)},
+  {path: 'Mainsite', loadComponent:()=>import('./components/mainsite/mainsite.component').then(c=> c.MainsiteComponent)} 
+];
---
+++ SportApplikation-FrontEnd/src/app/components/mainsite/mainsite.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mainsite',
+  imports: [],
+  templateUrl: './mainsite.component.html',
+  standalone: true,
+  styleUrl: './mainsite.component.css'
+})
+export class MainsiteComponent {
+
+}
