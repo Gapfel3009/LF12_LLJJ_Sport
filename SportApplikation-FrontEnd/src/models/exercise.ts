a8dc83ce3efa127646a09638a8f5ac8d59c42ca5
Luca <luca.w12@schule.bremen.de>
2025-05-07 21:19:01 +0200

    L12SPOR-22 Workout erstellen: Testdaten eingesetzt, Hinzufügen und Entfernen aus Workout, DargAndDrop für Reihenfolge der Übungen

--- SportApplikation-FrontEnd/src/models/exercise.ts
+++ SportApplikation-FrontEnd/src/models/exercise.ts
@@ -1,16 +1,44 @@
-export interface Exercise{
- ExerciseID: string;
- Name: string;
- Description: string;
- repetitions: number;
- sets: number;
- XP_Total: number;
- XP_breast: number;
- XP_Back: number;
- XP_leg: number;
- XP_triceps: number;
- XP_ass: number;
- XP_biceps: number;
- XP_flexibility: number;
- XP_shoulder: number;
+export class Exercise {
+  ExerciseID: number;
+  Name: string;
+  Description: string;
+  gifLink: string;
+  repetitions: number;
+  sets: number;
+  XP_Total: number;
+  XP_breast: number;
+  XP_Back: number;
+  XP_leg: number;
+  XP_triceps: number;
+  XP_ass: number;
+  XP_biceps: number;
+  XP_flexibility: number;
+  XP_shoulder: number;
+
+  //TODO: Constructor anpassen, erstmal für Testzwecke nur halb
+  constructor(ExerciseID: number,Name: string,Description: string,gifLink: string);
+  constructor();
+  constructor(
+    ExerciseID?: number,
+    Name?: string,
+    Description?: string,
+    gifLink?: string
+  ){
+  this.ExerciseID = ExerciseID ?? 0;
+  this.Name = Name ?? "";
+  this.Description = Description ?? "";
+  this.gifLink = gifLink ?? "";
+  this.repetitions = 0;
+  this.sets = 0;
+  this.XP_Total = 0;
+  this.XP_breast = 0;
+  this.XP_Back = 0;
+  this.XP_leg = 0;
+  this.XP_triceps = 0;
+  this.XP_ass = 0;
+  this.XP_biceps = 0;
+  this.XP_flexibility = 0;
+  this.XP_shoulder = 0;
 }
+}
+
