0a65a7a20e6a3aa8d6a3ca885f03ebf3142e545a
Lysander <lysander2004.lh@gmail.com>
2025-05-11 13:14:25 +0200

    L12SPOR-18 fügt der Klasse Workout Model Getter und Setter

--- SportApplication/src/main/java/de/szut/sportapplication/model/Workout.java
+++ SportApplication/src/main/java/de/szut/sportapplication/model/Workout.java
@@ -1,14 +1,14 @@
 package de.szut.sportapplication.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
 public class Workout {
 
     @Id
