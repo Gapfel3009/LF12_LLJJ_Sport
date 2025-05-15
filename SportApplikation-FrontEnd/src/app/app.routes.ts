d4201d658c90c6c96bbb98edbbc34772d6d2e270
Gott <hanpeter2003>
2025-05-15 08:58:57 +0200

    L12SPOR-22 Profile seite als component erstellt und redirect in mainsite eingesetzt

--- SportApplikation-FrontEnd/src/app/app.routes.ts
+++ SportApplikation-FrontEnd/src/app/app.routes.ts
@@ -4,5 +4,6 @@ export const routes: Routes = [
   {path: '', loadComponent:()=>import('./components/login/login.component').then(c=> c.LoginComponent)},
   {path: 'Mainsite', loadComponent:()=>import('./components/mainsite/mainsite.component').then(c=> c.MainsiteComponent)},
   {path: 'Workout-erstellen', loadComponent:() => import('./components/workout-erstellen/workout-erstellen.component').then(c =>c.WorkoutErstellenComponent)},
-  {path: 'Workouts', loadComponent:()=> import('./components/workouts/workouts.component').then(c=>c.WorkoutsComponent)}
+  {path: 'Workouts', loadComponent:()=> import('./components/workouts/workouts.component').then(c=>c.WorkoutsComponent)},
+  {path: 'Profile', loadComponent:()=> import('./components/profile/profile.component').then(c=>c.ProfileComponent)},
 ];
