7725ae2ef3643996ca5db0e1f2053c58c1549465
Luca <luca.w12@schule.bremen.de>
2025-05-15 08:19:44 +0200

    L12SPOR-22 workout.component erstellt

--- SportApplikation-FrontEnd/src/app/app.routes.ts
+++ SportApplikation-FrontEnd/src/app/app.routes.ts
@@ -3,5 +3,6 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {path: '', loadComponent:()=>import('./components/login/login.component').then(c=> c.LoginComponent)},
   {path: 'Mainsite', loadComponent:()=>import('./components/mainsite/mainsite.component').then(c=> c.MainsiteComponent)},
-  {path: 'Workout-erstellen', loadComponent:() => import('./components/workout-erstellen/workout-erstellen.component').then(c =>c.WorkoutErstellenComponent)}
+  {path: 'Workout-erstellen', loadComponent:() => import('./components/workout-erstellen/workout-erstellen.component').then(c =>c.WorkoutErstellenComponent)},
+  {path: 'Workouts', loadComponent:()=> import('./components/workouts/workouts.component').then(c=>c.WorkoutsComponent)}
 ];
