1da2f0fd98e4873437ff98eb48c1b314095064bd
Gott <hanpeter2003>
2025-05-27 18:15:35 +0200

    Allgemeiner Hover effekt für die Muskelgruppen, sortierung nach rück und front muskeln, aktuell alles hart codet kann man aber sicherlch dynamisch machen

--- SportApplikation-FrontEnd/src/app/components/profile/profile.component.ts
+++ SportApplikation-FrontEnd/src/app/components/profile/profile.component.ts
@@ -15,11 +15,25 @@ import {AppUser} from '../../../models/AppUser';
 export class ProfileComponent implements OnInit {
   constructor(private userService: UserService) {
   }
+  isfront: boolean =true;
+  currentUser: AppUser | null = null;
+  hoveredMuscle: string | null = null;
+
+  muscleLines: Record<string, { x1: number; y1: number; x2: number; y2: number }> = {
+    xpTotal: { x1: 10, y1: 10, x2: 150, y2: 10 },
+    xpBack: { x1: 10, y1: 100, x2: 150, y2: 100 },
+    xpShoulders: { x1: 10, y1: 60, x2: 150, y2: 60 },
+    xpLegs: { x1: 10, y1: 250, x2: 150, y2: 250 },
+    xpTriceps: { x1: 10, y1: 80, x2: 150, y2: 80 },
+    xpAbs: { x1: 10, y1: 140, x2: 150, y2: 140 },
+    xpGlutes: { x1: 10, y1: 300, x2: 150, y2: 300 },
+    xpBiceps: { x1: 10, y1: 50, x2: 150, y2: 50 }
+  };
+
   ngOnInit() {
     this.getUser()
   }
-  isfront: boolean =true;
-  currentUser: AppUser | null = null;
+
   FlipThePage(){
     if(this.isfront){
       this.isfront = false;
