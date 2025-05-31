a5a2202ee6a32827808b94e23b1b6df355bb0fd7
Gott <hanpeter2003>
2025-05-31 11:33:23 +0200

    Daten werden tatsächlich geändert. auf der Profil seite bekommt man visuellen input welcher avatar aktuell ausgewählt ist.

--- SportApplikation-FrontEnd/src/app/components/profile/profile.component.ts
+++ SportApplikation-FrontEnd/src/app/components/profile/profile.component.ts
@@ -2,24 +2,28 @@ import {Component, OnInit} from '@angular/core';
 import {UserService} from '../../services/user.service';
 import {AppUser} from '../../../models/AppUser';
 import {FormsModule} from '@angular/forms';
+import {NgClass} from '@angular/common';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-profile',
   imports: [
-    FormsModule
+    FormsModule,
+    NgClass
   ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent implements OnInit {
-  constructor(private userService: UserService) {
+  constructor(private userService: UserService, private router: Router) {
   }
   currentUser: AppUser | null = null;
-
+  AvaId: number = 1;
   newUsername: string = '';
 
   ngOnInit() {
     this.getUser()
+    this.AvaId = this.currentUser?.avatarID!
     console.log('currentUser', this.currentUser)
     console.log(this.userService.getCurrentUser())
   }
@@ -29,14 +33,19 @@ export class ProfileComponent implements OnInit {
   }
   setAvatarId(avatarID:number){
     this.currentUser!.avatarID = avatarID;
+    this.AvaId = avatarID;
     console.log(this.currentUser);
-    this.userService.updateUser(this.currentUser!);
+    this.userService.updateUser(this.currentUser!).subscribe();
   }
   UpdateUser(username: string){
     this.newUsername = username;
     this.currentUser!.username = this.newUsername;
     console.log(this.currentUser!);
-    this.userService.updateUser(this.currentUser!);
+    this.userService.updateUser(this.currentUser!).subscribe();
     console.log(this.userService.getCurrentUser())
+    this.MainRedirect();
+  }
+  MainRedirect(){
+    this.router.navigate(['/Mainsite']);
   }
 }
